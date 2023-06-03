4dcb49683b1be281fb28d2b6b36f97c191d6b5f7
Tony <pp.tony777@gmail.com>
2023-06-03 13:59:49 -0600

    Implementig menu for the user

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,40 +2,37 @@ import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/shared/user.service';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   @Input() loggedIn: boolean;
 
   isTransparent: boolean = true;
   isRegularUser: boolean = false;
+  showMenu: boolean = false;
 
-  constructor(private auth:UserService, private router: Router) { }
+  constructor(private auth: UserService, private router: Router) {}
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    // Verificar la posición del scroll
     if (window.pageYOffset > 50) {
-      // Si la posición del scroll es mayor que 50px, actualizar la propiedad 'isTransparent' a false
       this.isTransparent = false;
     } else {
-      // Si la posición del scroll es menor que 50px, actualizar la propiedad 'isTransparent' a true
       this.isTransparent = true;
     }
   }
 
-  ngOnInit() : void {
-    this.auth.loggedIn.subscribe(loggedIn => {
+  ngOnInit(): void {
+    this.auth.loggedIn.subscribe((loggedIn) => {
       if (loggedIn) {
         this.isRegularUser = true;
-        console.log(loggedIn + "Usuario logeado");
+        console.log(loggedIn + 'Usuario logeado');
       } else {
         this.isRegularUser = false;
-        console.log(loggedIn + "Null");
+        console.log(loggedIn + 'Null');
         if (this.isRegularUser) {
           this.router.navigate(['/login']);
         }
@@ -43,10 +40,16 @@ export class HeaderComponent implements OnInit{
     });
   }
 
-  logOut(){
-    this.auth.logOut()
-    .then(() =>{
-      this.router.navigate(['/home']);
-    }).catch(e => console.log(e));
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  logOut() {
+    this.auth
+      .logOut()
+      .then(() => {
+        this.router.navigate(['/home']);
+      })
+      .catch((e) => console.log(e));
   }
 }
