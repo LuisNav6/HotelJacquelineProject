566811770907d0590d41f00f14e63a1cb2af0e02
Tony <pp.tony777@gmail.com>
2023-06-09 16:20:32 -0600

    Email work 100% (its only send emails)

--- src/app/components/contact-us/form-book/form-book.component.ts
+++ src/app/components/contact-us/form-book/form-book.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import * as Notiflix from 'notiflix';
 
 @Component({
   selector: 'app-form-book',
@@ -11,25 +13,61 @@ export class FormBookComponent implements OnInit{
   faInstagram = faInstagram;
   faFacebook = faFacebook;
   faTwitter = faTwitter;
-  contactForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
-    this.contactForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', Validators.required],
-      message: ['', Validators.required]
-    });
-  }
+  receiveDatadMsg: any = {};
+  okBack: boolean = false;
+  showReceivedData: boolean = false;
+  data: FormGroup;
   
-  onSubmit() {
-    if (this.contactForm.valid) {
-      console.log(this.contactForm.value);
-      // Enviar el formulario a través de una petición HTTP
-    } else {
-      console.log("Formulario inválido");
+  // Inyectamos HttpClient para establecer la conexion con el back-end
+  constructor(private httpClient: HttpClient) {
+    // Incializamos el FormGroup
+    this.data = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      about: new FormControl('', Validators.required),
+      mssg: new FormControl('', Validators.required)
+    })
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  sendEmail() {
+    Notiflix.Loading.circle('Loading...');
+    // Recuperamos los datos del form
+    let params = {
+      email: this.data.value.email,
+      about: this.data.value.about,
+      mssg: this.data.value.mssg
     }
+
+    // Establecemos la conexión con el backend
+    console.log(params);
+    this.httpClient.post('https://test-email-api.onrender.com/send', params).subscribe((res: any) => {
+      console.log(res);
+      Notiflix.Loading.remove();
+      this.data.reset();
+      if (res.ok) {
+        Notiflix.Notify.success('Successfully sent');
+        this.okBack = res.ok;
+        this.receiveDatadMsg = JSON.parse(res.msg);  // Guardamos los datos recibidos en `receivedData`
+        console.log(this.receiveDatadMsg);
+        // Puedes realizar cualquier acción adicional con los datos recibidos, como mostrarlos en el frontend
+      } else {
+        // La solicitud no fue exitosa, puedes realizar acciones específicas para manejar errores
+        this.okBack = res.ok;
+        Notiflix.Notify.failure('Something went wrong. Try again...');
+        console.log('Error en la respuesta del servidor');
+      }
+    })
+  }
+
+  status() {
+    if(this.okBack ){
+      this.showReceivedData = true;
+    }else{
+      Notiflix.Notify.failure('No data available.');
+    }
+    
   }
 }
