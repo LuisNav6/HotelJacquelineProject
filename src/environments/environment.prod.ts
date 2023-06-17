2475287bb834f644353aa21c3279739de3fd9161
Tony <pp.tony777@gmail.com>
2023-06-17 05:18:20 -0600

    production enabled prod again

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBkyYzflxqwTaYAjWVhu7i_4AauN0ZS8Mg",
+    authDomain: "reservaciones-adf27.firebaseapp.com",
+    projectId: "reservaciones-adf27",
+    storageBucket: "reservaciones-adf27.appspot.com",
+    messagingSenderId: "268825628920",
+    appId: "1:268825628920:web:7312e147059ed23f3f6b0d",
+    measurementId: "G-FVPDBQF2FZ"
+  }
 };
