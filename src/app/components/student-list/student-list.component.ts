a60168bf3452135bc9c542c5edb42b6bb3cb4256
Tony <pp.tony777@gmail.com>
2023-06-17 21:46:00 -0600

    Adding qr to admin

--- src/app/components/student-list/student-list.component.ts
+++ src/app/components/student-list/student-list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../../shared/crud.service';
 import { User } from '../../shared/student';
 import { ToastrService } from 'ngx-toastr';
-
+import { QrService } from 'src/app/shared/qr.service';
+import QRCode from 'qrcode';
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
@@ -14,8 +15,12 @@ export class StudentListComponent implements OnInit {
   hideWhenNoStudent: boolean = false;
   noData: boolean = false;
   preLoader: boolean = true;
+  qrData: any;
+  qrCodeUrl: string;
+  qrData2: any;
+  qrCodeUrl2: string;
 
-  constructor(public crudApi: CrudService, public toastr: ToastrService) {}
+  constructor(public crudApi: CrudService, public toastr: ToastrService, private qr:QrService) {}
 
   ngOnInit() {
     this.dataState();
@@ -43,9 +48,65 @@ export class StudentListComponent implements OnInit {
   }
 
   deleteUser(user) {
-    if (window.confirm('Are you sure you want to delete this student?')) {
+    if (window.confirm('Are you sure you want to delete this reservation?')) {
       this.crudApi.DeleteUser(user.$key);
       this.toastr.success(user.firstName + ' successfully deleted!');
     }
+  }
+
+  generar(user){
+    this.qr.obtenerReserva(user.email).subscribe(
+      (registros) => {
+        this.qrData = JSON.stringify(registros);
+        this.generarQRCode();
+        this.toastr.success("QR Code of " + user.firstName + " " + user.lastName + " was successfully generated");
+      },
+      (error) => {
+        console.error('Error al obtener los registros:', error);
+      }
+    );
+  }
+
+  generarGeneral(){
+    this.qr.obtenerRegistros().subscribe(
+      (registros) => {
+        this.qrData2 = JSON.stringify(registros);
+        this.generarQRCode2();
+      },
+      (error) => {
+        console.error('Error al obtener los registros:', error);
+      }
+    );
+  }
+  generarQRCode() {
+    const qrCodeOptions = {
+      width: 200,
+      height: 200,
+      type: 'image/png',
+    };
+
+    QRCode.toDataURL(this.qrData, qrCodeOptions, (err, url) => {
+      if (err) {
+        console.error('Error al generar el código QR:', err);
+      } else {
+        this.qrCodeUrl = url;
+      }
+    });
+  }
+
+  generarQRCode2() {
+    const qrCodeOptions = {
+      width: 200,
+      height: 200,
+      type: 'image/png',
+    };
+
+    QRCode.toDataURL(this.qrData2, qrCodeOptions, (err, url) => {
+      if (err) {
+        console.error('Error al generar el código QR:', err);
+      } else {
+        this.qrCodeUrl2 = url;
+      }
+    });
   }
 }
